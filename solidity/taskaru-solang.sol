efad42990d83adeb0c24d883ddefd0b5e1dd9cfd
purrnama <purrnama@proton.me>
2023-09-30 22:22:04 +0800

    latest smart contract

--- solidity/taskaru-solang.sol
+++ solidity/taskaru-solang.sol
@@ -1,42 +1,60 @@
 
-@program_id("F1ipperKF9EfD821ZbbYjS319LXYiBmjhzkkf5a26rC")
+@program_id("8cTeqUjgcLhLhD6nzVoioJ8sbDnitsz86TtFSNjyrVnr")
 contract taskaru_solang {
     //data structs
-    UserScore private accountData;
-    struct UserScore {
-        address player;
-        uint64 currentScore;
-        uint64 highestScore;
+    struct UserPortfolio {
+        address user;
+        uint64 tasksCompleted;
+        bool isAcceptedTask;
+        bool isSubmittedSolution;
         bytes1 bump;
     }
 
+    struct TaskStatus {
+        bool isAcceptedTask;
+        bool isSubmittedSolution;
+    }
+
+    UserPortfolio public accountData;
+    
     //account initializer
     @payer(payer)
     @seed("seed")
-    constructor(@seed bytes payer, @bump bytes1 bump, address player){
-        print("New UserScore account initialized");
-        accountData = UserScore (player, 0, 0, bump);
+    constructor(@seed bytes payer, @bump bytes1 bump, address user){
+        print("New UserPortfolio account initialized");
+        accountData = UserPortfolio (user, 0, false, false, bump);
     }
 
     //functions
     //setters
-    function addPoints(uint8 numPoints) public {
-        require(numPoints > 0 && numPoints < 100, 'INVALID POINTS STOOPID');
-        accountData.currentScore += numPoints;
-        if (accountData.currentScore > accountData.highestScore) {
-            accountData.highestScore = accountData.currentScore;
-        }
+    function setIsAcceptedTask(bool value) public {
+        require(accountData.isSubmittedSolution == false, "user already submitted solution");
+        require(accountData.isAcceptedTask != value, "attempt to assign same value");
+        accountData.isAcceptedTask = value;
     }
-
-    function resetScore() public {
-        accountData.currentScore = 0;
+    function submitSolution() public {
+        require(accountData.isAcceptedTask == true, "user is not participating in the task");
+        require(accountData.isSubmittedSolution == false, "attempt to submit another solution");
+        accountData.isSubmittedSolution = true;
+        accountData.tasksCompleted += 1;
+    }
+    function resetAll() public {
+        accountData.tasksCompleted = 0;
+        accountData.isAcceptedTask = false;
+        accountData.isSubmittedSolution = false;
     }
 
     //getters
-    function getCurrentScore() public view returns (uint64) {
-        return accountData.currentScore;
+    function getTasksCompleted() public view returns (uint64) {
+        return accountData.tasksCompleted;
     }
-    function getHighestScore() public view returns (uint64) {
-        return accountData.highestScore;
+    function getIsAcceptedTask() public view returns (bool) {
+        return accountData.isAcceptedTask;
+    }
+    function getIsSubmittedSolution() public view returns (bool) {
+        return accountData.isSubmittedSolution;
+    }
+    function getTaskStatus() public view returns (TaskStatus) {
+        return TaskStatus(accountData.isAcceptedTask, accountData.isSubmittedSolution);
     }
 }
--- tests/taskaru-solang.ts
+++ tests/taskaru-solang.ts
@@ -4,10 +4,6 @@ import { PublicKey } from "@solana/web3.js";
 import { assert, expect } from "chai";
 import { TaskaruSolang } from "../target/types/taskaru_solang";
 
-function randomPointsGenerator(min = 1, max = 100) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 describe("taskaru-solang", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -19,50 +15,124 @@ describe("taskaru-solang", () => {
     program.programId
   );
 
-  it("Test 1 - Initializes a new account!", async () => {
+  const keypair = anchor.web3.Keypair.generate();
+  const seed = keypair.publicKey.toBuffer();
+  const [dataAccount2, bump2] = PublicKey.findProgramAddressSync(
+    [Buffer.from("seed"), seed],
+    program.programId
+  );
+
+  it("Initializes a new account!", async () => {
     const tx = await program.methods
       .new(walletSeed, [bump], dataAccount)
-      .accounts({ dataAccount })
+      .accounts({ dataAccount: dataAccount })
       .rpc();
-    const score = await program.methods
-      .getCurrentScore()
-      .accounts({ dataAccount })
+    const value = await program.methods
+      .getTasksCompleted()
+      .accounts({ dataAccount: dataAccount })
       .view();
-    expect(score.toNumber()).to.equal(0);
+    expect(value.toNumber()).to.equal(0);
+  });
+  it("User accepts task!", async () => {
+    await program.methods
+      .setIsAcceptedTask(true)
+      .accounts({ dataAccount: dataAccount })
+      .rpc();
+    const result = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    expect(result).to.equal(true);
+  });
+  it("User forfeits task!", async () => {
+    await program.methods
+      .setIsAcceptedTask(false)
+      .accounts({ dataAccount: dataAccount })
+      .rpc();
+    const result = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    expect(result).to.equal(false);
   });
 
-  it("Test 2 - Adds points!", async () => {
-    const pointsToAdd = randomPointsGenerator();
-    await program.methods
-      .addPoints(pointsToAdd)
-      .accounts({ dataAccount })
+  it("Initializes another account!", async () => {
+    const tx = await program.methods
+      .new(seed, [bump2], dataAccount2)
+      .accounts({ dataAccount: dataAccount2 })
       .rpc();
-    const score = await program.methods
-      .getCurrentScore()
-      .accounts({ dataAccount })
+    const value = await program.methods
+      .getTasksCompleted()
+      .accounts({ dataAccount: dataAccount2 })
       .view();
-    expect(score.toNumber()).to.equal(pointsToAdd);
+    expect(value.toNumber()).to.equal(0);
   });
-
-  it("Test 3 - Resets and checks high score!", async () => {
-    const initialScore = (
-      await program.methods.getCurrentScore().accounts({ dataAccount }).view()
-    ).toNumber();
-    const pointsToAdd = randomPointsGenerator();
+  it("Second user accepts task!", async () => {
     await program.methods
-      .addPoints(pointsToAdd)
-      .accounts({ dataAccount })
+      .setIsAcceptedTask(true)
+      .accounts({ dataAccount: dataAccount2 })
       .rpc();
-    await program.methods.resetScore().accounts({ dataAccount }).rpc();
-    const updatedScore = await program.methods
-      .getCurrentScore()
-      .accounts({ dataAccount })
+    const result = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount2 })
       .view();
-    expect(updatedScore.toNumber()).to.equal(0);
-    const highScore = await program.methods
-      .getHighestScore()
-      .accounts({ dataAccount })
+    expect(result).to.equal(true);
+  });
+  it("isAcceptedTask values are unique!", async () => {
+    const value1 = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount })
       .view();
-    expect(highScore.toNumber()).to.equal(initialScore + pointsToAdd);
+    const value2 = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount2 })
+      .view();
+    expect(value1 == value2).to.equal(false);
+  });
+  it("Second user submits a solution!", async () => {
+    const tx = await program.methods
+      .submitSolution()
+      .accounts({ dataAccount: dataAccount2 })
+      .rpc();
+    const result = await program.methods
+      .getIsSubmittedSolution()
+      .accounts({ dataAccount: dataAccount2 })
+      .view();
+    expect(result).to.equal(true);
+  });
+  it("User gets task status!", async () => {
+    const value = await program.methods
+      .getTaskStatus()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    console.log(value);
+  });
+  it("Second user gets task status!", async () => {
+    const value = await program.methods
+      .getTaskStatus()
+      .accounts({ dataAccount: dataAccount2 })
+      .view();
+    console.log(value);
+  });
+  it("User resets all data!!? Das crazy", async () => {
+    const tx = await program.methods
+      .resetAll()
+      .accounts({ dataAccount: dataAccount })
+      .rpc();
+    const completedTasks = await program.methods
+      .getTasksCompleted()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    const isAcceptedTask = await program.methods
+      .getIsAcceptedTask()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    const isSubmittedSolution = await program.methods
+      .getIsSubmittedSolution()
+      .accounts({ dataAccount: dataAccount })
+      .view();
+    expect(completedTasks.toNumber()).to.equal(0);
+    expect(isAcceptedTask).to.equal(false);
+    expect(isSubmittedSolution).to.equal(false);
   });
 });
